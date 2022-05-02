d8865c39cc5601af557737bd61edc7fbf80fad2d
SelumFuu <luis.ezequiel.10@gmail.com>
2022-05-02 16:27:03 -0400

    V 0.1

--- EjercicioHebra/Program.cs
+++ EjercicioHebra/Program.cs
@@ -1,7 +1,9 @@
-﻿using System;
+﻿using EjercicioHebra.Hebras;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace EjercicioHebra
@@ -10,6 +12,18 @@ namespace EjercicioHebra
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Iniciando Hebras");
+            for (int i = 1; i < 7; i++)
+            {
+                //Construir una clase que representa mi hebra y pasar los paramentros
+                HebraEjercicio he = new HebraEjercicio(i);
+                //Ejecuto el metodo de instancia en la hebra nueva
+                Thread t = new Thread(new ThreadStart(he.ejecutar));
+                t.Start();
+            }
+
+            Console.WriteLine("Hebra iniciada");
+            Console.ReadKey();
         }
     }
 }
